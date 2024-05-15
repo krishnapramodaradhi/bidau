d3f307ce2a211fd420aaee682530fbd2f3569dc0
Krishna Pramod <pramod@mac.local>
2024-05-15 16:47:38 +0530

    feat: Added comments for better readability

--- auction/handler.go
+++ auction/handler.go
@@ -9,39 +9,45 @@ import (
 	"time"
 )
 
-const bidPriceLowerThreshold float64 = 29.99
-
 type BiddingResponse struct {
 	AdId     string  `json:"adId"`
 	BidPrice float64 `json:"bidPrice"`
+	Err      error   `json:"-"`
 }
 
 var availableBidders = []string{"http://bidding:8080/buy/ad-slot", "http://bidding-1:8080/buy/ad-slot", "http://bidding-2:8080/buy/ad-slot"}
 
 func HeaderBidHandler(w http.ResponseWriter, r *http.Request) {
+	// Extract the adPlacementId from path params
 	adPlacementId := r.URL.Path[len("/bid/"):]
 	log.Println("Request for Ad Placement", adPlacementId)
 
 	var highestBid float64
 	wg := sync.WaitGroup{}
-	individualBids := make(chan float64, len(availableBidders))
+	individualBids := make(chan BiddingResponse, len(availableBidders))
 	defer close(individualBids)
 
+	// initializing a context with 200ms timeout
 	ctx, cancel := context.WithTimeout(context.Background(), time.Duration(200*time.Millisecond))
 	defer cancel()
 
+	// Iterates over the available bidders and makes an http request to them in a goroutine
 	for _, bidder := range availableBidders {
 		wg.Add(1)
 		go fetchAdBid(bidder, &wg, individualBids, ctx)
 	}
+
+	// extract the bidPrice from the channel and select the highest bid
 	go func() {
-		for bidPrice := range individualBids {
-			if highestBid < bidPrice {
-				highestBid = bidPrice
+		for bid := range individualBids {
+			if highestBid < bid.BidPrice {
+				highestBid = bid.BidPrice
 			}
 		}
 	}()
 	wg.Wait()
+
+	// if no eligible bid is found, send a 204
 	if highestBid == 0 {
 		w.WriteHeader(http.StatusNoContent)
 		return
@@ -49,9 +55,11 @@ func HeaderBidHandler(w http.ResponseWriter, r *http.Request) {
 	writeJSON(w, http.StatusOK, highestBid)
 }
 
-func fetchAdBid(bidder string, wg *sync.WaitGroup, bidCh chan float64, ctx context.Context) {
+// Returns the bid response from the bid-service.
+func fetchAdBid(bidder string, wg *sync.WaitGroup, bidCh chan BiddingResponse, ctx context.Context) {
 	var response BiddingResponse
 	defer wg.Done()
+
 	req, err := http.NewRequestWithContext(ctx, http.MethodGet, bidder, nil)
 	if err != nil {
 		log.Println("An error occured in handler", err)
@@ -68,6 +76,10 @@ func fetchAdBid(bidder string, wg *sync.WaitGroup, bidCh chan float64, ctx conte
 		if err := json.NewDecoder(res.Body).Decode(&response); err != nil {
 			log.Println("An error occured in handler", err)
 		}
-		bidCh <- response.BidPrice
+		bidCh <- BiddingResponse{
+			AdId:     response.AdId,
+			BidPrice: response.BidPrice,
+			Err:      err,
+		}
 	}
 }
